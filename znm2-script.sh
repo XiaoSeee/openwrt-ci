bd4193f659bd0fff8547e7dbab08c43e28c5a131
XiaoSeee <420961648@qq.com>
2025-03-17 17:24:40 +0800

    调整菜单标签

--- znm2-script.sh
+++ znm2-script.sh
@@ -5,9 +5,14 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 
 # 添加 timecontrol
 git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-timecontrol.git package/luci-app-timecontrol
+sed -i 's/"admin", "control"/"admin", "services"/g' package/luci-app-timecontrol/luci-app-nft-timecontrol/luasrc/controller/timecontrol.lua
+sed -i 's/"Control"/"Services"/g' package/luci-app-timecontrol/luci-app-nft-timecontrol/luasrc/controller/timecontrol.lua
+sed -i 's/"admin", "control"/"admin", "services"/g' package/luci-app-timecontrol/luci-app-nft-timecontrol/luasrc/view/timecontrol/index.htm
 
-# 更改 Argon 主题背景
-# cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# 修改 openvpn-server 至‘’标签
+sed -i 's/"admin", "vpn"/"admin", "services"/g' package/feeds/luci/luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
+sed -i 's/"VPN"/"Services"/g' package/feeds/luci/luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
+sed -i 's/"admin", "vpn"/"admin", "services"/g' package/feeds/luci/luci-app-openvpn-server/luasrc/view/openvpn/openvpn_status.htm
 
 # 修改版本为编译日期
-sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
