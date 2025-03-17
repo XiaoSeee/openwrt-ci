64c4e607b2a3479e57bb6dba4fd8efefb0971735
XiaoSeee <420961648@qq.com>
2025-03-17 10:09:37 +0800

    Revert "添加 Parent Control"
    This reverts commit 03a05f0c5e06ce1a61fbe0f9edcbe8e368debadd.

--- znm2-script.sh
+++ znm2-script.sh
@@ -3,9 +3,6 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
-# 添加 Parent Control
-git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-parentcontrol.git package/luci-app-parentcontrol
-
 # 更改 Argon 主题背景
 # cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
