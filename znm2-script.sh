adb14c80b730a71b265e296db6476bfaabfcee01
XiaoSeee <420961648@qq.com>
2026-02-02 14:23:25 +0800

    同步编译修改

--- znm2-script.sh
+++ znm2-script.sh
@@ -3,12 +3,6 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
-# 添加 timecontrol
-git clone --depth 1 --single-branch https://github.com/sirpdboy/luci-app-timecontrol.git package/luci-app-timecontrol
-sed -i 's/"admin", "control"/"admin", "services"/g' package/luci-app-timecontrol/luci-app-nft-timecontrol/luasrc/controller/timecontrol.lua
-sed -i 's/"Control"/"Services"/g' package/luci-app-timecontrol/luci-app-nft-timecontrol/luasrc/controller/timecontrol.lua
-sed -i 's/"admin", "control"/"admin", "services"/g' package/luci-app-timecontrol/luci-app-nft-timecontrol/luasrc/view/timecontrol/index.htm
-
 # 修改 openvpn-server 至‘’标签
 sed -i 's/"admin", "vpn"/"admin", "services"/g' package/feeds/luci/luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
 sed -i 's/"VPN"/"Services"/g' package/feeds/luci/luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
